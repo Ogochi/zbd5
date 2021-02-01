13552664c4f267a96f1485c276c081d5be054c71
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2021-02-01 23:54:32 +0100

    Added initial data & postgres connection fix

--- advert_info/database.py
+++ advert_info/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql://postgres:admin1@localhost/postgres')
+engine = create_engine('postgresql://postgres:admin1@postgres/postgres')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
--- init.sql
+++ init.sql
@@ -31,3 +31,31 @@ CREATE TABLE advert_display(
     advert_id uuid not null references advert,
     display_time timestamp null
 );
+
+INSERT INTO uuser
+VALUES
+  ('01aa0956-1afc-4e97-9df6-487f95a190d6', 'M', 123, 60, 60.1),
+  ('7fc99b4c-a51c-4f73-8362-a3d8c9638416', 'M', 12123, 60.2, 60.3),
+  ('e3bf931e-07db-4ddf-aaf0-c3e8586e0322', 'F', 1238888, 50.321, 60.1);
+
+INSERT INTO user_interest
+VALUES
+  (1, '01aa0956-1afc-4e97-9df6-487f95a190d6', 'cyberpunk');
+
+INSERT INTO advert
+VALUES
+  ('01aa0956-1afc-4e97-9df6-487f95a190d6', 10, 21, '#A03F2A'),
+  ('7fc99b4c-a51c-4f73-8362-a3d8c9638416', 11, 20, '#957F7A'),
+  ('e3bf931e-07db-4ddf-aaf0-c3e8586e0322', 12, 1238888, '#7A8E95');
+
+INSERT INTO advert_text
+VALUES
+  (1, '01aa0956-1afc-4e97-9df6-487f95a190d6', 'aaa'),
+  (2, '7fc99b4c-a51c-4f73-8362-a3d8c9638416', 'ccc'),
+  (3, 'e3bf931e-07db-4ddf-aaf0-c3e8586e0322', 'xxx');
+
+INSERT INTO advert_display
+VALUES
+  (1, '01aa0956-1afc-4e97-9df6-487f95a190d6', '7fc99b4c-a51c-4f73-8362-a3d8c9638416', '2006-01-02T16:04:05'),
+  (2, '7fc99b4c-a51c-4f73-8362-a3d8c9638416', '7fc99b4c-a51c-4f73-8362-a3d8c9638416', '2006-02-02T16:04:05'),
+  (3, 'e3bf931e-07db-4ddf-aaf0-c3e8586e0322', 'e3bf931e-07db-4ddf-aaf0-c3e8586e0322', '2006-03-02T16:04:05');
