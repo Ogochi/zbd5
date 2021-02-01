ad4454adcf667a575489b1f8e81f8924e125ca7a
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2021-02-01 17:27:33 +0100

    Added db schema

--- init.sql
+++ init.sql
@@ -1,4 +1,30 @@
-CREATE TABLE audience(
-    file_date date not null primary key,
-    content jsonb not null
+CREATE TABLE uuser(
+    id uuid primary key not null,
+    gender char(1) null,
+    yearly_income bigint null,
+    longitude float null,
+    latitude float null
+);
+
+CREATE TABLE user_interests(
+    user_id uuid not null references uuser,
+    interest varchar(50) not null
+);
+
+CREATE TABLE advert(
+    id uuid primary key not null,
+    height int null,
+    width int null,
+    main_color varchar(20) null
+);
+
+CREATE TABLE advert_text(
+    advert_id uuid not null references advert,
+    text_content text not null
+);
+
+CREATE TABLE advert_display(
+    user_id uuid not null references uuser,
+    advert_id uuid not null references advert,
+    display_time timestamp null
 );
